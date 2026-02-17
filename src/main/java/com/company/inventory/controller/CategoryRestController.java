e5df267eae3a0bad6e1aaa1adb5468d70fb974bd
Banibarza <banirafael19@gmail.com>
2026-02-17 13:33:04 -0600

    CORS CONFIG

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import com.company.inventory.services.ICategoryService;
 import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
-
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
@@ -76,3 +77,4 @@ public class CategoryRestController {
         return response;
     }
 }
+ 
